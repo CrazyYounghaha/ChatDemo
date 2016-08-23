4858aeda45945f32c1655f5607c312e3ea8c34a4
CrazyYounghaha <568524975@qq.com>
2016-08-23 17:32:17 +0800

    8-23 add password md5 add user register

--- src/common/bootstrap/global.js
+++ src/common/bootstrap/global.js
@@ -10,3 +10,26 @@
  *     
  * }
  */
+
+
+/**
+ * 密码加密
+ * @param password 加密的密码
+ * @param md5encoded true-密码不加密，默认加密
+ * @returns {*}
+ */
+global.encryptPassword = function(password, md5encoded) {
+		md5encoded = md5encoded || false;
+		password = md5encoded ? password : think.md5(password);
+		return think.md5( password + think.md5('Arterli'));
+};
+
+/**
+ * ltrim()
+ * @param str [删除左边的空格]
+ * @returns {*|void|string|XML}
+ */
+/* global ltrim */
+global.ltrim = function(str) {
+		return str.replace(/(^\s*)/g, "");
+}
--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -3,43 +3,74 @@
 import Base from './base.js';
 
 export default class extends Base {
-  /**
-   * index action
-   * @return {Promise} []
-   */
-  indexAction(){
-    //auto render template file index_index.html
-    this.assign("title", "ThinkJs 官网");
-    return this.display();
-  }
-  loginAction(){
-    if(this.isPost()){
-      let name = this.post('name');
-      let pwd = this.post('pwd');
-      let user = this.model('user').where({name: name}).find().then(
-          (obj) => {
-            if( pwd == obj.pwd){
-              console.log("success");
-              this.session('loginuser',obj);
-              this.success(obj);
-            }else {
-              console.log("error");
-              this.error(obj);
-            }
-          });//.catch((error) => { console.log(error.message+ '???')})
-    }
-  }
-  * logoutAction(){
-    if(this.islogin){
-      yield this.session('loginuser',null);
-      console.log("1");
-      return this.redirect('index');
-    }
-  }
-  * afterloginAction(){
-    let data = yield this.session('loginuser');
-    //console.log(data);
-    this.assign('username',data);
-    return this.display("login");
-  }
+		/**
+		 * index action
+		 * @return {Promise} []
+		 */
+		indexAction() {
+				//auto render template file index_index.html
+				this.assign("title", "ThinkJs 官网");
+				return this.display();
+		}
+
+		* loginAction() {
+				if (this.isPost()) {
+						let name = this.post('name');
+						let pwd = this.post('pwd');
+						pwd = encryptPassword(pwd);
+						let user = yield this.model('user').where({name: name}).find();
+
+						if (pwd == user.pwd) {
+								console.log("Login success");
+								yield this.session('loginuser', user);
+								return this.success(user);
+						} else {
+								console.log("error");
+								user.err = 1;
+								return this.success(user);
+						}
+				}
+		}
+
+		* registerAction() {
+				if (this.isAjax('post')) {
+						let data = this.post();
+						console.log(data);
+						if (think.isEmpty(data.r_name)) {
+								return this.fail("用户昵称不能为空！");
+						} else {
+								let res = yield this.model("user").where({name: ltrim(data.r_name)}).find();
+								if (!think.isEmpty(res)) {
+										return this.fail("用户昵称已存在，请重新填写！")
+								}
+						}
+						if (think.isEmpty(data.r_pwd) && think.isEmpty(data.pwdConfirm)) {
+								return this.fail("密码不能为空！")
+						} else {
+								if (data.r_pwd != data.pwdConfirm) {
+										return this.fail("两次输入的密码不一致，请重新填写！")
+								}
+						}
+						data.reg_time = new Date().valueOf();
+						data.r_pwd = encryptPassword(data.r_pwd);
+						let reg = yield this.model("user").add({name: data.r_name, pwd: data.r_pwd});
+						let userInfo = {
+								'id': reg,
+								'username': data.r_name,
+								'last_login_time': data.reg_time
+						};
+						yield this.session('loginuser', userInfo);
+						return this.success({name: "注册成功！登录中", url: "/index"});
+				} else {
+						return this.display();
+				}
+		}
+
+		* logoutAction() {
+				if (this.islogin) {
+						yield this.session('loginuser', null);
+						return this.redirect('index');
+				}
+		}
+
 }
